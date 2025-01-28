3f6a17d90dd6566f110a5575e9df0dd7001986e0
saurabh7nt <saurabhtajne07@gmail.com>
2025-01-28 09:55:16 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_demo_server" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
-  key_name      = Demo_key
+  key_name      = "Demo_key"
   tags = {
     Name = "ExampleAppDemoServerInstance"
   }
